eedd841e3d964897c39cc2a7648b5773c9bab9e7
xribene <xribene55@gmail.com>
2023-05-28 12:49:26 -0400

    changed confing_players structure

--- src/components/Mixer.vue
+++ src/components/Mixer.vue
@@ -35,6 +35,7 @@
         return {
             pane: null,
             data: [],
+            data2: {},
             playersConfig: null,
             title: "",
         };
@@ -59,6 +60,7 @@
             let vm = this;
             vm.playersConfig = playersConfig;
             // vm.title = mixerConfig.title;
+            // vm.data2 = vm.playersConfig;
 
             vm.pane = new Pane({
               container: vm.$refs.mixer,
@@ -68,58 +70,107 @@
             //     console.log('changed: ' + JSON.stringify(ev.value));
             // });
             
-            vm.playersConfig.forEach((player) => {
-                let playerName = player.label;
-                vm.data[`${playerName}mute`] = player.mute;
-                vm.data[`${playerName}volume`] = player.volume;
-                player.instruments.forEach((instrument) => {
-                    vm.data[`${playerName}${instrument.name}mute`] = instrument.mute;
-                    vm.data[`${playerName}${instrument.name}volume`] = instrument.volume;
+            for (const [player, playerData] of Object.entries(vm.playersConfig)) {
+                // let playerName = player.label;
+                // vm.data[`${playerName}mute`] = player.mute;
+                // vm.data[`${playerName}volume`] = player.volume;
+                vm.data2[`${player}`] = {
+                    label: playerData.label,
+                    mute: playerData.mute,
+                    volume: playerData.volume,
+                    instruments: {}
+                };
+                playerData.instruments.forEach((instrument) => {
+                    console.log(instrument.id + " " + instrument.label)
+                    vm.data2[`${player}`].instruments[`${instrument.id}`] = {
+                        label: instrument.label,
+                        mute: instrument.mute,
+                        volume: instrument.volume,
+                    };
+                    // vm.data[`${playerName}${instrument.name}mute`] = instrument.mute;
+                    // vm.data[`${playerName}${instrument.name}volume`] = instrument.volume;
                 });
-            });
+            };
 
-            // Iterate over the structure in the config file
-            vm.playersConfig.forEach((player) => {
-            // Create a folder for each tab
+            // Iterate over the 'players' in the config file
+            for (const [player, playerData] of Object.entries(vm.data2)) {
+                console.log(player, " ", playerData);
                 const playerFolder = vm.pane.addFolder({
-                    title: player.label,
+                    title: playerData.label,
                     expanded: true,
                 });
-                
-                let playerName = player.label;
-                playerFolder.addInput(vm.data, `${playerName}volume`,
-                {
-                    label: "volume",
-                    min: 1,
-                    max: 10
-                });
-                playerFolder.addInput(vm.data, `${playerName}mute`,
-                {
-                    label: "mute",
-                });
-
-                console.log("instuments for player " + player.label + " are " + player.instruments)
-                player.instruments.forEach((instrument) => {
-                    console.log(instrument.name);
-                    // player.instruments.forEach((instrument) => {
-                    const instFolder = playerFolder.addFolder({
-                        title: instrument.name,
-                        // expanded: true,
-                    });
-                    console.log("added sub")
-                    instFolder.addInput(vm.data, `${playerName}${instrument.name}volume`,
+                playerFolder.addInput(vm.data2[player], 'volume',
                     {
-                        label: "mute",
+                        label: "vol.",
                         min: 1,
                         max: 10
                     });
-                    instFolder.addInput(vm.data, `${playerName}${instrument.name}mute`,
+                playerFolder.addInput(vm.data2[player], 'mute',
                     {
                         label: "mute",
                     });
-                    // });
-                });
-            });
+                for (const [instId, instData] of Object.entries(playerData.instruments)) {
+                    console.log(instData.label);
+                    // player.instruments.forEach((instrument) => {
+                    const instFolder = playerFolder.addFolder({
+                        title: instData.label,
+                        expanded: false,
+                    });
+                    console.log("added sub")
+                    instFolder.addInput(vm.data2[player].instruments[instId], 'volume',
+                    {
+                        label: "vol.",
+                        min: 1,
+                        max: 10
+                    });
+                    instFolder.addInput(vm.data2[player].instruments[instId], 'mute',
+                    {
+                        label: "mute",
+                    });
+                };
+            }
+
+            // vm.playersConfig.forEach((player) => {
+            // // Create a folder for each tab
+            //     const playerFolder = vm.pane.addFolder({
+            //         title: player.label,
+            //         expanded: true,
+            //     });
+                
+            //     let playerName = player.label;
+            //     playerFolder.addInput(vm.data, `${playerName}volume`,
+            //     {
+            //         label: "vol.",
+            //         min: 1,
+            //         max: 10
+            //     });
+            //     playerFolder.addInput(vm.data, `${playerName}mute`,
+            //     {
+            //         label: "mute",
+            //     });
+
+            //     console.log("instuments for player " + player.label + " are " + player.instruments)
+            //     player.instruments.forEach((instrument) => {
+            //         console.log(instrument.name);
+            //         // player.instruments.forEach((instrument) => {
+            //         const instFolder = playerFolder.addFolder({
+            //             title: instrument.name,
+            //             // expanded: true,
+            //         });
+            //         console.log("added sub")
+            //         instFolder.addInput(vm.data, `${playerName}${instrument.name}volume`,
+            //         {
+            //             label: "vol.",
+            //             min: 1,
+            //             max: 10
+            //         });
+            //         instFolder.addInput(vm.data, `${playerName}${instrument.name}mute`,
+            //         {
+            //             label: "mute",
+            //         });
+            //         // });
+            //     });
+            // });
         },
         updateData() {
             // Modify the local copy of the propData and emit an event
@@ -130,28 +181,28 @@
   
   };
   </script>
-  <!-- <style>
+  <style>
   :root {
-    --tp-base-background-color: hsla(0, 0%, 10%, 0.8);
-    --tp-base-shadow-color: hsla(0, 0%, 0%, 0.2);
-    --tp-button-background-color: hsla(0, 0%, 80%, 1);
+    --tp-base-background-color: hsla(0, 9%, 22%, 0.863);
+    --tp-base-shadow-color: hsla(44, 78%, 24%, 0.247);
+    /* --tp-button-background-color: hsla(0, 0%, 80%, 1); */
     --tp-button-background-color-active: hsla(0, 0%, 100%, 1);
     --tp-button-background-color-focus: hsla(0, 0%, 95%, 1);
     --tp-button-background-color-hover: hsla(0, 0%, 85%, 1);
     --tp-button-foreground-color: hsla(0, 0%, 0%, 0.8);
-    --tp-container-background-color: hsla(0, 0%, 0%, 0.3);
-    --tp-container-background-color-active: hsla(0, 0%, 0%, 0.6);
-    --tp-container-background-color-focus: hsla(0, 0%, 0%, 0.5);
-    --tp-container-background-color-hover: hsla(0, 0%, 0%, 0.4);
+    /* --tp-container-background-color: hsla(0, 0%, 0%, 0.795); */
+    --tp-container-background-color-active: hsla(0, 0%, 0%, 0.781);
+    --tp-container-background-color-focus: hsla(0, 0%, 0%, 0.719);
+    --tp-container-background-color-hover: hsla(36, 89%, 26%, 0.4); 
     --tp-container-foreground-color: hsla(0, 0%, 100%, 0.5);
-    --tp-groove-foreground-color: hsla(0, 0%, 0%, 0.2);
-    --tp-input-background-color: hsla(0, 0%, 0%, 0.3);
+    --tp-groove-foreground-color: hsla(0, 78%, 51%, 0.2);
+    /* --tp-input-background-color: hsla(0, 81%, 46%, 0.822); */
     --tp-input-background-color-active: hsla(0, 0%, 0%, 0.6);
     --tp-input-background-color-focus: hsla(0, 0%, 0%, 0.5);
-    --tp-input-background-color-hover: hsla(0, 0%, 0%, 0.4);
-    --tp-input-foreground-color: hsla(0, 0%, 100%, 0.5);
-    --tp-label-foreground-color: hsla(0, 0%, 100%, 0.5);
+    --tp-input-background-color-hover: hsla(45, 72%, 14%, 0.541);
+    --tp-input-foreground-color: hsla(41, 92%, 42%, 0.911);
+    --tp-label-foreground-color: hsla(39, 53%, 86%, 0.945);
     --tp-mixer-background-color: hsla(0, 0%, 0%, 0.3);
     --tp-mixer-foreground-color: hsla(0, 0%, 100%, 0.3);
   }
-  </style> -->
+  </style>
