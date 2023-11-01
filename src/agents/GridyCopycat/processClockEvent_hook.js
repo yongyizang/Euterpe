4fd274d51cd7b8b140ea356ef338055fca8b12b8
xribene <xribene@yahoo.com>
2023-11-01 11:01:16 -0400

    simplify gridyCopycat

--- src/agents/GridyCopycat/processClockEvent_hook.js
+++ src/agents/GridyCopycat/processClockEvent_hook.js
@@ -1,18 +1,32 @@
 import {NoteEvent} from '@/utils/NoteEvent.js';
 
+// variables local to this file
 let lastNote = null;
 let prevStartTime = performance.now();
 
-/**
- *
- * @param {Object} content
- * @return {Object}
- */
+function createNote(midi, type) {
+    const noteOn = new NoteEvent();
+    noteOn.player = self.playerType.AGENT;
+    noteOn.instrument = self.instrumentType.PIANO;
+    noteOn.type = type;
+    noteOn.midi = midi;
+    noteOn.velocity = 127;
+    // Play it instantly
+    noteOn.playAfter = {
+        tick: self.delay,
+        seconds: 0,
+    };
+    return noteOn;
+}
+
 export function processClockEvent(content) {
+
+    // Calculations for monitoring the true BPM
     const startTime = performance.now();
     const timeDiff = startTime - prevStartTime;
     prevStartTime = startTime;
 
+    // Get the clock-aligned user note events from the content
     const quantizedEvents = content.humanQuantizedInput;
     const noteList = [];
     const message = {};
@@ -22,75 +36,32 @@ export function processClockEvent(content) {
     const noteHoldEvents = quantizedEvents.filter((note) => note.type === self.noteType.NOTE_HOLD);
     if (noteOnEvents.length > 0) {
         if (lastNote) {
-            // create a note off event
-            const noteOffResponse = new NoteEvent();
-            noteOffResponse.player = self.playerType.AGENT;
-            noteOffResponse.instrument = self.instrumentType.PIANO;
-            noteOffResponse.type = self.noteType.NOTE_OFF;
-            noteOffResponse.midi = lastNote.midi;
-            noteOffResponse.velocity = lastNote.velocity;
-            noteOffResponse.playAfter = {
-                tick: self.delay,
-                seconds: 0,
-            };
-            noteList.push(noteOffResponse);
+            // This means there is alread a note on, so we need to turn it off
+            // we do that to ensure our agent outputs a monophonic melody
+            // which is necessary for the score visualization to work
+            noteList.push(createNote(lastNote.midi, self.noteType.NOTE_OFF));
             lastNote = null;
         }
-
         const currentNote = noteOnEvents[0];
-        const noteOnResponse = new NoteEvent();
-        noteOnResponse.player = self.playerType.AGENT;
-        noteOnResponse.instrument = self.instrumentType.PIANO;
-        noteOnResponse.type = currentNote.type;
-        noteOnResponse.midi = currentNote.midi + self.pitchShift + Math.floor(Math.random() * self.randomness);
-        noteOnResponse.velocity = currentNote.velocity;
-        noteOnResponse.playAfter = {
-            tick: self.delay,
-            seconds: 0,
-        },
+        let outputMidi = currentNote.midi + self.pitchShift + Math.floor(Math.random() * self.randomness);
+        const noteOnResponse = createNote(outputMidi, self.noteType.NOTE_ON)
         noteList.push(noteOnResponse);
+        // update the lastNote variable
         lastNote = noteOnResponse;
+        // update the user-to-agent note mapping
+        // we'll use that to turn off the note later
         self.userToAgentNoteMapping[currentNote.midi] = noteOnResponse.midi;
-        console.log('agent generated note: ' + noteOnResponse.midi);
-    } else if (noteOnEvents.length === 0 && noteHoldEvents.length > 0) {
-        if (lastNote) {
-            // console.log(noteHoldEvents[0].midi, " ", lastNote.midi);
-            // if (lastNote.midi + 12 !== noteHoldEvents[0].midi) {
-            if (lastNote.midi !== self.userToAgentNoteMapping[noteHoldEvents[0].midi]) {
-                // create a note off event
-                const noteOffResponse = new NoteEvent();
-                noteOffResponse.player = self.playerType.AGENT;
-                noteOffResponse.instrument = self.instrumentType.PIANO;
-                noteOffResponse.type = self.noteType.NOTE_OFF;
-                noteOffResponse.midi = lastNote.midi;
-                noteOffResponse.velocity = lastNote.velocity;
-                noteOffResponse.playAfter = {
-                    tick: self.delay,
-                    seconds: 0,
-                };
-                noteList.push(noteOffResponse);
-                lastNote = null;
-            }
-        }
     } else if (noteOnEvents.length === 0 && noteHoldEvents.length === 0) {
+        // If there is no note on or hold event, it means the user has 
+        // released all the notes, so we need to turn off the last note
         if (lastNote) {
             // create a note off event
-            const noteOffResponse = new NoteEvent();
-            noteOffResponse.player = self.playerType.AGENT;
-            noteOffResponse.instrument = self.instrumentType.PIANO;
-            noteOffResponse.type = self.noteType.NOTE_OFF;
-            noteOffResponse.midi = lastNote.midi;
-            noteOffResponse.velocity = lastNote.velocity;
-            noteOffResponse.playAfter = {
-                tick: self.delay,
-                seconds: 0,
-            };
-            noteList.push(noteOffResponse);
+            noteList.push(createNote(lastNote.midi, self.noteType.NOTE_OFF));
             lastNote = null;
         }
     }
 
-
+    // calculations for monitoring the true BPM
     const actualPeriod = timeDiff;
     const actualBPM = 60 / (actualPeriod / 1000) / self.config.clockSettings.ticksPerBeat;
     console.log(actualBPM);
